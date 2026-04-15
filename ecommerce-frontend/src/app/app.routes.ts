98c188a346352a230691e5b7b747bbb7d07c1684
matei1998 <mateiserband@gmail.com>
2026-04-15 17:22:45 +0300

    add cart and order services & routing setup

--- ecommerce-frontend/src/app/app.routes.ts
+++ ecommerce-frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ProductList } from './components/product-list/product-list';
+import { Cart } from './components/cart/cart';
+import { Checkout } from './components/checkout/checkout';
+import { Register } from './components/register/register';
+import { Login } from './components/login/login';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductList },
+  { path: 'cart', component: Cart },
+  { path: 'checkout', component: Checkout, canActivate: [authGuard] },
+  { path: 'register', component: Register },
+  { path: 'login', component: Login }
+];
