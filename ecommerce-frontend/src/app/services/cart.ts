5ab89597899761e7ff3b6f765b0a38a5ea103276
matei1998 <mateiserband@gmail.com>
2026-04-16 14:25:24 +0300

    complete cart and order services

--- ecommerce-frontend/src/app/services/cart.ts
+++ ecommerce-frontend/src/app/services/cart.ts
@@ -1,6 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, tap } from 'rxjs';
+import { CartItem, CartItemDto } from '../models/models';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Cart {}
+export class CartService {
+  private apiUrl = 'http://localhost:5284/api/cart';
+
+  private cartItems = new BehaviorSubject<CartItem[]>([]);
+  cartItems$ = this.cartItems.asObservable();
+
+  constructor(private http: HttpClient) {}
+
+  loadCart() {
+    return this.http.get<CartItem[]>(this.apiUrl)
+      .pipe(tap(items => this.cartItems.next(items)));
+  }
+
+  addToCart(productId: number, quantity: number = 1) {
+    return this.http.post(this.apiUrl, { productId, quantity } as CartItemDto)
+      .pipe(tap(() => this.loadCart().subscribe()));
+  }
+
+  updateQuantity(productId: number, quantity: number) {
+    return this.http.put(`${this.apiUrl}/${productId}`, { productId, quantity } as CartItemDto)
+      .pipe(tap(() => this.loadCart().subscribe()));
+  }
+
+  removeFromCart(productId: number) {
+    return this.http.delete(`${this.apiUrl}/${productId}`)
+      .pipe(tap(() => this.loadCart().subscribe()));
+  }
+
+  clearLocal() {
+    this.cartItems.next([]);
+  }
+
+  getItemCount(): number {
+    return this.cartItems.value.reduce((sum, item) => sum + item.quantity, 0);
+  }
+}
--- ecommerce-frontend/src/app/services/order.ts
+++ ecommerce-frontend/src/app/services/order.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CheckoutRequest, Order } from '../models/models';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Order {}
+export class OrderService {
+  private apiUrl = 'http://localhost:5284/api/orders';
+
+  constructor(private http: HttpClient) {}
+
+  checkout(shippingAddress: string) {
+    return this.http.post<Order>(`${this.apiUrl}/checkout`, { shippingAddress } as CheckoutRequest);
+  }
+
+  getOrders() {
+    return this.http.get<Order[]>(this.apiUrl);
+  }
+}
